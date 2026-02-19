f0235ddf00087124157c238bde8bf98002c76384
axle <sir.axle@yandex.ru>
2026-02-19 23:21:02 +0300

    Add rococo-grpc-common module with artist.proto and protobuf generation

---
+++ rococo-artist/src/test/java/guru/qa/rococoartist/RococoArtistApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.qa.rococoartist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RococoArtistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
