902bff640c60123a611b338074581798ab69a700
axle <sir.axle@yandex.ru>
2026-03-13 10:18:09 +0300

    feat(painting): init

---
+++ rococo-painting/src/test/java/guru/qa/rococopainting/RococoPaintingApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.qa.rococopainting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RococoPaintingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
