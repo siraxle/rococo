2ad3d765736e87acdacbd89d54e692fbb3943ae2
axle <sir.axle@yandex.ru>
2026-03-14 13:04:57 +0300

    feat(geo): init

---
+++ rococo-geo/src/main/java/guru/qa/rococogeo/RococoGeoApplication.java
@@ -0,0 +1,13 @@
+package guru.qa.rococogeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RococoGeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RococoGeoApplication.class, args);
+    }
+
+}
