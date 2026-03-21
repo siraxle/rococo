692aa70e300bcd04bec74d2eb9c2e8990f4df9cf
axle <sir.axle@yandex.ru>
2026-03-21 15:19:00 +0300

    rococo-tests: add annotations

---
+++ rococo-tests/src/main/java/guru/qa/jupiter/annotation/Painting.java
@@ -0,0 +1,13 @@
+package guru.qa.jupiter.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Painting {
+    String title() default "";
+    String description() default "";
+}
