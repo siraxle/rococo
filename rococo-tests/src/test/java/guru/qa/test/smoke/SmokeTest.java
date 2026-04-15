2181b7ffedd0f53010ab4c100919e41a97831aa3
axle <sir.axle@yandex.ru>
2026-04-15 11:30:54 +0300

    add test-with-services.yml, SmokeTest

---
+++ rococo-tests/src/test/java/guru/qa/test/smoke/SmokeTest.java
@@ -0,0 +1,12 @@
+package guru.qa.test.smoke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTest {
+
+    @Test
+    void smokeTest() {
+        assertTrue(true);
+    }
+}
